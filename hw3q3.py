f19c425fcfea1d2554a17e0211a986bfcba149a6
allisonrenie <allisonrenie@gmail.com>
2021-04-25 19:07:50 -0700

    validates input, only allows postive numbers

--- hw3q3.py
+++ hw3q3.py
@@ -1,4 +1,28 @@
-num = input("Please enter a year: ")
+# Homework 1 Question 4
+# Programmed by Allison Thompson
+# Run my program by navigating to its location with the Command Prompt and
+# typing in the full file name including .py. The program will then prompt you
+# to type in a year. The program will prompt you until you enter a valid
+# positive number. The program will then tell you if the year you entered
+# is a leap year or not.
+
+# get num from user
+num = 0
+isInt = False
+while True:
+    num = input("Please enter a year: ")
+    # checking if the input can be converted to an int
+    try:
+        int(num)
+    # if we get a ValueError, the num couldn't be converted, and is not an int
+    except ValueError:
+        continue
+    # checking if the input is above 0
+    if int(num) < 1:
+        continue
+    num = int(num)
+    break
+
 # is num divisible by 4?
 divByFour = int(num % 4)
 if divByFour == 0:
